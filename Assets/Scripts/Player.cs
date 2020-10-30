8fd6169eced6ac9c93c6b33df1c8a71810a98cc1
Arman Rizal <inazrabuu@gmail.com>
2020-10-30 20:28:46 +0700

    player controller input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnMoveUp(InputAction.CallbackContext context)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMoveDown(InputAction.CallbackContext context)
     {
-        
+
+    }
+
+    public void OnMoveLeft(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnMoveRight(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnAttackUp(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnAttackDown(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnAttackLeft(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnAttackRight(InputAction.CallbackContext context)
+    {
+
     }
 }
