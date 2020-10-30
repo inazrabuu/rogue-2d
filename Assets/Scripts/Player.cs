ab9cb2c9867cd18a217409ef6df2236f74ef63c2
Arman Rizal <inazrabuu@gmail.com>
2020-10-30 20:52:39 +0700

    player controller move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,24 +5,51 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public int curHp, maxHp, coins;
+    public bool hasKey;
+    public SpriteRenderer spriteRenderer;
+
+    public LayerMask moveLayerMask;
+
+    public void Move(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 1f, moveLayerMask);
+        if (hit.collider == null)
+        {
+            transform.position += new Vector3(direction.x, direction.y, 0);
+        }
+    }
+
     public void OnMoveUp(InputAction.CallbackContext context)
     {
-
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Move(Vector2.up);
+        }
     }
 
     public void OnMoveDown(InputAction.CallbackContext context)
     {
-
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Move(Vector2.down);
+        }
     }
 
     public void OnMoveLeft(InputAction.CallbackContext context)
     {
-
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Move(Vector2.left);
+        }
     }
 
     public void OnMoveRight(InputAction.CallbackContext context)
     {
-
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Move(Vector2.right);
+        }
     }
 
     public void OnAttackUp(InputAction.CallbackContext context)
