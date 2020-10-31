96688ede9bfb0fbe2fc39100c140811fbd9c883f
Arman Rizal <inazrabuu@gmail.com>
2020-10-31 12:35:03 +0700

    * Generation.cs: * Assembly-CSharp.csprojAssemblyReference.cache: generation script
    * ArtifactDB:
    * SourceAssetDB:
    * Assembly-CSharp.csproj:
    * UserPrefs.xml:
    * SampleScene.unity:
    * Assembly-CSharp.dll:
    * Assembly-CSharp.pdb:
    * c40575-mainStage.json:
    * Assembly-CSharp-Debug.json:

---
+++ Assets/Scripts/Generation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generation : MonoBehaviour
+{
+    public int mapWidth = 7;
+    public int mapHeight = 7;
+    public int roomsToGeneratee = 12;
+    public GameObject roomPrefab;
+
+    private int _roomCount;
+    private bool _roomsInstantiated;
+    private Vector2 _firstRoomPos;
+    private bool[,] _map;
+    private List<Room> _roomObject = new List<Room>();
+
+    public static Generation Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Generate()
+    {
+
+    }
+
+    void CheckRoom(int x, int y, int remaining, Vector2 generalDirection, bool firstRoom = false)
+    {
+
+    }
+
+    void InstantiateRooms()
+    {
+
+    }
+
+    void CalculateKeyAndExit()
+    {
+
+    }
+}
